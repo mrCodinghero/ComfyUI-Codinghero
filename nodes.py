91e5ed9ba2dd859aeb3210f8c38254d3e0c1206d
mrCodinghero <165808204+mrCodinghero@users.noreply.github.com>
2025-11-23 19:38:56 -0800

    Not so serious...

--- nodes.py
+++ nodes.py
@@ -149,11 +149,11 @@ class UpscaleSettingsCalc:
 
 
 #
-# VideoSettings
+# Settings (Basic)
 #
-# All the relevant video settings in one place, with automatic length * fps math.  Only length and fps are required.
+# All the basic settings in one convenient node.
 #
-class VideoSettings:
+class SettingsBasic:
 
     @classmethod
     def INPUT_TYPES(cls):
@@ -161,46 +161,69 @@ class VideoSettings:
             "optional": {
                 "width": ("INT", {"label": "width"}, {"default": "720"}),
                 "height": ("INT", {"label": "height"}, {"default": "480"}),
-                "length": ("INT", {"label": "length"}, {"default": "3"}),
-                "fps": ("FLOAT", {"label": "fps"}, {"default": "16.0"}),
                 "shift": ("FLOAT", {"label": "shift"}, {"default": "7.0"}),
                 "cfg": ("FLOAT", {"label": "cfg"}, {"default": "2.0"}),
                 "steps": ("INT", {"label": "steps"}, {"default": "4"}),
-                "switch": ("INT", {"label": "switch"}, {"default": "2"}),
                 "sampler_name": (comfy.samplers.KSampler.SAMPLERS,),
                 "scheduler": (comfy.samplers.KSampler.SCHEDULERS,),
-                "seed": ("INT", {"default": 0, "min": -1, "max": 2**63 - 1})
+                "seed": ("INT", {"default": 0, "min": -1, "max": 2**63 - 1}),
+                "resize": (
+                    ["none", "original", "nearest"],
+                    {"label": "Resize"},
+                    {"default": "none"}
+                ),
+                "image": ("IMAGE", {"default": None})
             }
         }
 
-    RETURN_TYPES = ("INT", "INT", "INT", "FLOAT", "FLOAT", "FLOAT", "INT", "INT", comfy.samplers.KSampler.SAMPLERS, comfy.samplers.KSampler.SCHEDULERS, "INT")
-    RETURN_NAMES = ("WIDTH", "HEIGHT", "FRAMES", "FPS", "SHIFT", "CFG", "STEPS", "SWITCH", "SAMPLER", "SCHEDULER", "SEED")
+    RETURN_TYPES = ("INT", "INT", "FLOAT", "INT", "FLOAT", comfy.samplers.KSampler.SAMPLERS, comfy.samplers.KSampler.SCHEDULERS, "INT")
+    RETURN_NAMES = ("WIDTH", "HEIGHT", "SHIFT", "STEPS", "CFG", "SAMPLER", "SCHEDULER", "SEED")
 
     FUNCTION = "process"
     CATEGORY = "custom"
 
-    def process(self, width, height, length, fps, shift, steps, switch, cfg, sampler_name, scheduler, seed):
-        if length is None or length == 0 or fps is None or fps < 1.0:
-            return (720, 480, 17, 16.0, 5.0, 2.5, 4, 2, 0)
+    def process(self, width, height, shift, steps, cfg, sampler_name, scheduler, seed, resize, image=None):
+        # generate a random seed if it's -1
+        if seed == -1:
+            seed = random.randint(0, 4294967294)
+
+        # don't do anything else if we don't have an image
+        if image is not None and len(image) > 0:
+            # get the image size
+            imgHeight, imgWidth = image.shape[1], image.shape[2]
+
+            match resize:
+                case "none":
+                    width  = width
+                    height = height
+                case "original":
+                    width  = imgWidth
+                    height = imgHeight
+                case "nearest":
+                    d = max(width, height)
+                    m = max(imgWidth, imgHeight)
+                    f = d / m
+                    width  = imgWidth * f
+                    height = imgHeight * f
+                case _:
+                    width  = width
+                    height = height
+
+            # return at least a single pixel
+            width  = max(1, roundIt(width))
+            height = max(1, roundIt(height))
 
         # adjust width and height to a multiple of 16
         width = round(width / 16) * 16
         height = round(height / 16) * 16
 
-        # do the math and add an extra frame
-        frames = roundIt((length * fps) + 1)
-
-        # generate a random seed if it's -1
-        if seed == -1:
-            seed = random.randint(0, 4294967294)
-
-        return (width, height, frames, fps, shift, cfg, steps, switch, sampler_name, scheduler, seed)
+        return (width, height, shift, cfg, steps, sampler_name, scheduler, seed)
 
 
 #
 # Settings
 #
-# All the relevant settings in one convenient node.
+# All the settings in one convenient node.
 #
 class Settings:
 
@@ -219,9 +242,9 @@ class Settings:
                 "sampler_name": (comfy.samplers.KSampler.SAMPLERS,),
                 "scheduler": (comfy.samplers.KSampler.SCHEDULERS,),
                 "seed": ("INT", {"default": 0, "min": -1, "max": 2**63 - 1}),
-                "scaling": (
-                    ["none", "fixed", "nearest"],
-                    {"label": "Scaling"},
+                "resize": (
+                    ["none", "original", "nearest"],
+                    {"label": "Resize"},
                     {"default": "none"}
                 ),
                 "image": ("IMAGE", {"default": None})
@@ -234,13 +257,9 @@ class Settings:
     FUNCTION = "process"
     CATEGORY = "custom"
 
-    def process(self, width, height, length, fps, shift, steps, switch, cfg, sampler_name, scheduler, seed, scaling, image=None):
+    def process(self, width, height, length, fps, shift, steps, switch, cfg, sampler_name, scheduler, seed, resize, image=None):
         if length is None or length == 0 or fps is None or fps < 1.0:
             return (720, 480, 17, 16.0, 5.0, 2.5, 4, 2, 0)
-
-        # adjust width and height to a multiple of 16
-        width = round(width / 16) * 16
-        height = round(height / 16) * 16
 
         # do the math and add an extra frame
         frames = roundIt((length * fps) + 1)
@@ -254,11 +273,11 @@ class Settings:
             # get the image size
             imgHeight, imgWidth = image.shape[1], image.shape[2]
 
-            match scaling:
+            match resize:
                 case "none":
                     width  = width
                     height = height
-                case "fixed":
+                case "original":
                     width  = imgWidth
                     height = imgHeight
                 case "nearest":
@@ -275,6 +294,10 @@ class Settings:
             width  = max(1, roundIt(width))
             height = max(1, roundIt(height))
 
+        # adjust width and height to a multiple of 16
+        width = round(width / 16) * 16
+        height = round(height / 16) * 16
+
         return (width, height, frames, fps, shift, cfg, steps, switch, sampler_name, scheduler, seed)
 
 
@@ -283,7 +306,7 @@ NODE_CLASS_MAPPINGS = {
     "Model Selector": ModelSelector,
     "Image Size Calculator": ImageSizeCalc,
     "Upscale Settings Calculator": UpscaleSettingsCalc,
-    "Video Settings": VideoSettings,
+    "Basic Settings": SettingsBasic,
     "Settings": Settings
 }
 
@@ -291,6 +314,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "Model Selector": "Model Selector",
     "Image Size Calculator": "Image Size Calculator",
     "Upscale Settings Calculator": "Upscale Settings Calculator",
-    "Video Settings": "Video Settings",
+    "Basic Settings": "Basic Settings",
     "Settings": "Settings"
 }
